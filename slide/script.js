f5a615ba04ac1ccfbd5342a36c92ca9f237ff815
sagargarg <sagargarg@gmail.com>
2016-02-17 20:40:00 -0500

    Slider

---
+++ slide/script.js
@@ -0,0 +1,19 @@
+var rangeSlider = function(){
+  var slider = $('.range-slider');
+  var range = $('.range-slider_range');
+  var value = $('.range-slider_value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+
+rangeSlider();
