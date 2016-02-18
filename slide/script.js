1d83a57d6430980b0e8e7a64fb39b601e9ea6b0e
sagargarg <sagargarg@gmail.com>
2016-02-17 21:05:08 -0500

    3 sliders

--- slide/script.js
+++ slide/script.js
@@ -1,14 +1,18 @@
 var rangeSlider = function(){
-	var range = $('.range-slider_range');
+	var slider = $('.range-slider');
+	var range = $('.range-slider_range')
 	var value = $('.range-slider_value');
 		
-	value.each(function(){
-		var value = $(this).prev().attr('value');
-		$(this).html(value);
-	});
+	slider.each(function(){
 
-	range.on('input', function(){
-		$(this).next(value).html(this.value);
+		value.each(function(){
+			var value = $(this).prev().attr('value');
+			$(this).html(value);
+		});
+
+		range.on('input', function(){
+			$(this).next(value).html(this.value);
+		});
 	});
 };
 
