7ab5a1d201c4b8e26c2dace53a2be18a55d6e4d3
sagargarg <sagargarg@gmail.com>
2016-02-17 20:59:39 -0500

    I fixed it

--- slide/script.js
+++ slide/script.js
@@ -1,19 +1,15 @@
 var rangeSlider = function(){
-  var slider = $('.range-slider');
-  var range = $('.range-slider_range');
-  var value = $('.range-slider_value');
-    
-  slider.each(function(){
+	var range = $('.range-slider_range');
+	var value = $('.range-slider_value');
+		
+	value.each(function(){
+		var value = $(this).prev().attr('value');
+		$(this).html(value);
+	});
 
-    value.each(function(){
-      var value = $(this).prev().attr('value');
-      $(this).html(value);
-    });
-
-    range.on('input', function(){
-      $(this).next(value).html(this.value);
-    });
-  });
+	range.on('input', function(){
+		$(this).next(value).html(this.value);
+	});
 };
 
 rangeSlider();
